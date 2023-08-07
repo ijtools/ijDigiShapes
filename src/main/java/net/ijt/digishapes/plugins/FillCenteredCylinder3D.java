75096bf3a6a29e43e729bcf9239d7d5c7a0af8dd
dlegland <david.legland@inrae.fr>
2023-08-07 12:18:52 +0200

    add CenteredCylinder3D.java

--- src/main/java/net/ijt/digishapes/plugins/FillCenteredCylinder3D.java
+++ src/main/java/net/ijt/digishapes/plugins/FillCenteredCylinder3D.java
@@ -10,16 +10,17 @@ import ij.ImagePlus;
 import ij.ImageStack;
 import ij.gui.GenericDialog;
 import ij.plugin.PlugIn;
-import net.ijt.geom3d.Bounds3D;
-import net.ijt.geom3d.surface.Ellipsoid3D;
+import net.ijt.digishapes.shapes3d.CenteredCylinder3D;
+import net.ijt.geom3d.Point3D;
 
 /**
- * Fills an ellipsoid defined by user parameters on the current 3D image.
+ * Fills a 3D cylinder defined by center and three rotation angles on the
+ * current 3D image.
  * 
  * @author dlegland
  *
  */
-public class FillEllipsoid3D implements PlugIn
+public class FillCenteredCylinder3D implements PlugIn
 {
     @Override
     public void run(String arg)
@@ -38,16 +39,15 @@ public class FillEllipsoid3D implements PlugIn
         int sizeZ = array.getSize();
         
         // create the dialog
-        GenericDialog gd = new GenericDialog("Fill Ellipsoid");
+        GenericDialog gd = new GenericDialog("Fill Centered Cylinder");
         gd.addNumericField("Center_X", Math.floor(sizeX / 2), 2);
         gd.addNumericField("Center_Y", Math.floor(sizeY / 2), 2);
         gd.addNumericField("Center_Z", Math.floor(sizeZ / 2), 2);
-        gd.addNumericField("Semi-Axis_1", 40, 2);
-        gd.addNumericField("Semi-Axis_2", 20, 2);
-        gd.addNumericField("Semi-Axis_3", 10, 2);
-        gd.addNumericField("Euler_Angle_X (degrees)", 0, 2);
-        gd.addNumericField("Euler_Angle_Y (degrees)", 0, 2);
-        gd.addNumericField("Euler_Angle_Z (degrees)", 0, 2);
+        gd.addNumericField("Length", 50, 2);
+        gd.addNumericField("Radius", 10, 2);
+        gd.addNumericField("Euler Angle X (degrees)", 0, 2);
+        gd.addNumericField("Euler Angle Y (degrees)", 0, 2);
+        gd.addNumericField("Euler Angle Z (degrees)", 0, 2);
         gd.addNumericField("Fill Value", 255, 0);
 
         gd.showDialog();
@@ -55,12 +55,11 @@ public class FillEllipsoid3D implements PlugIn
             return;
         
         // retrieve user inputs
-        double centerX = gd.getNextNumber();
-        double centerY = gd.getNextNumber();
-        double centerZ = gd.getNextNumber();
-        double semiAxis1 = gd.getNextNumber();
-        double semiAxis2 = gd.getNextNumber();
-        double semiAxis3 = gd.getNextNumber();
+        double cx = gd.getNextNumber();
+        double cy = gd.getNextNumber();
+        double cz = gd.getNextNumber();
+        double length = gd.getNextNumber();
+        double radius = gd.getNextNumber();
         double eulerAngleX = gd.getNextNumber();
         double eulerAngleY = gd.getNextNumber();
         double eulerAngleZ = gd.getNextNumber();
@@ -69,26 +68,26 @@ public class FillEllipsoid3D implements PlugIn
         long t0 = System.currentTimeMillis();
 
         // generate geometric shape
-        Ellipsoid3D elli = new Ellipsoid3D(centerX, centerY, centerZ, semiAxis1, semiAxis2, semiAxis3, eulerAngleX, eulerAngleY, eulerAngleZ);
+        Point3D center = new Point3D(cx, cy, cz);
+        CenteredCylinder3D cyl = new CenteredCylinder3D(center, length, radius, eulerAngleX, eulerAngleY, eulerAngleZ);
         
-        // computation bounds
-        Bounds3D bounds = elli.bounds();
-        int x0 = Math.max((int) Math.floor(bounds.minX()), 0);
-        int x1 = Math.min((int) Math.ceil(bounds.maxX()), sizeX - 1);
-        int y0 = Math.max((int) Math.floor(bounds.minY()), 0);
-        int y1 = Math.min((int) Math.ceil(bounds.maxY()), sizeY - 1);
-        int z0 = Math.max((int) Math.floor(bounds.minZ()), 0);
-        int z1 = Math.min((int) Math.ceil(bounds.maxZ()), sizeZ - 1);
-
+//        // computation bounds
+//        Bounds3D bounds = cyl.bounds();
+//        int x0 = Math.max((int) Math.floor(bounds.minX()), 0);
+//        int x1 = Math.min((int) Math.ceil(bounds.maxX()), sizeX - 1);
+//        int y0 = Math.max((int) Math.floor(bounds.minY()), 0);
+//        int y1 = Math.min((int) Math.ceil(bounds.maxY()), sizeY - 1);
+//        int z0 = Math.max((int) Math.floor(bounds.minZ()), 0);
+//        int z1 = Math.min((int) Math.ceil(bounds.maxZ()), sizeZ - 1);
         
         // iterate over image voxels
-        for (int z = z0; z <= z1; z++)
+        for (int z = 0; z < sizeZ; z++)
         {
-            for (int y = y0; y <= y1; y++)
+            for (int y = 0; y < sizeY; y++)
             {
-                for (int x = x0; x <= x1; x++)
+                for (int x = 0; x < sizeX; x++)
                 {
-                    if (elli.isInside(x + 0.5, y + 0.5, z + 0.5))
+                    if (cyl.isInside(x + 0.5, y + 0.5, z + 0.5))
                     {
                         array.setVoxel(x, y, z, fillValue);
                     }
@@ -106,7 +105,7 @@ public class FillEllipsoid3D implements PlugIn
         
         // show elapsed time
         String pattern = "%s: %.3f seconds";
-        String status = String.format(Locale.ENGLISH, pattern, "Fill Ellipsoid", timeInSecs);
+        String status = String.format(Locale.ENGLISH, pattern, "Fill Cylinder", timeInSecs);
         IJ.showStatus(status);
     }
 }
---
+++ src/main/java/net/ijt/digishapes/shapes3d/CenteredCylinder3D.java
@@ -0,0 +1,132 @@
+/**
+ * 
+ */
+package net.ijt.digishapes.shapes3d;
+
+import net.ijt.geom3d.AffineTransform3D;
+import net.ijt.geom3d.Point3D;
+import net.ijt.geom3d.Rotation3D;
+
+/**
+ * @author dlegland
+ *
+ */
+public class CenteredCylinder3D
+{
+    // ===================================================================
+    // Class variables
+
+    /**
+     * The center of the cylinder.
+     */
+    final Point3D center;
+    
+    final double length;
+    
+    final double radius;
+    
+    /**
+     * The first rotation applied to the cylinder, around the X-axis, in degrees.
+     */
+    final double eulerAngleX; 
+    /**
+     * The second rotation applied to the cylinder, around the Y-axis, in degrees.
+     */
+    final double eulerAngleY; 
+    /**
+     * The third rotation applied to the cylinder, around the Z-axis, in degrees.
+     */
+    final double eulerAngleZ;
+    
+    
+    // ===================================================================
+    // Constructors
+    
+    public CenteredCylinder3D(Point3D center, double length, double radius)
+    {
+        this(center, length, radius, 0, 0, 0);
+    }
+
+    public CenteredCylinder3D(Point3D center, double length, double radius, double eulerX, double eulerY, double eulerZ)
+    {
+        this.center = center;
+        this.length = length;
+        this.radius = radius;
+        this.eulerAngleX = eulerX;
+        this.eulerAngleY = eulerY;
+        this.eulerAngleZ = eulerZ;
+    }
+    
+    /**
+     * Returns the orientation of this cylinder, as a Rotation3D object. 
+     * 
+     * @return the orientation of the cylinder.
+     */
+    public Rotation3D orientation()
+    {
+        return Rotation3D.fromEulerAngles(Math.toRadians(eulerAngleX), Math.toRadians(eulerAngleY), Math.toRadians(eulerAngleZ));
+    }
+    
+
+    /**
+     * Checks if the specified point is contained within the domain bounded by
+     * this boundary.
+     * 
+     * @param point
+     *            the point to test
+     * @return true is the point is within the domain corresponding to this
+     *         boundary.
+     */
+    public boolean isInside(Point3D point)
+    {
+        Point3D pt = globalToLocalTransform().transform(point);
+        double r = Math.hypot(pt.x(), pt.y());
+        if (r > 1) return false;
+        if (Math.abs(pt.z()) > 0.5) return false;
+        return true;
+    }
+
+    /**
+     * Checks if the specified point is contained within the domain bounded by
+     * this boundary.
+     * 
+     * @param x
+     *            the x-coordinate of the point to test
+     * @param y
+     *            the y-coordinate of the point to test
+     * @return true is the point is within the domain corresponding to this
+     *         boundary.
+     */
+    public boolean isInside(double x, double y, double z)
+    {
+        return isInside(new Point3D(x, y, z));
+    }
+    
+    /**
+     * Creates the affine transform that will map a centered unit cube to this
+     * cylinder instance.
+     * 
+     * @return the affine transform that will map a centered unit cube to this
+     *cylindercuboid instance.
+     */
+    private AffineTransform3D localToGlobalTransform()
+    {
+        // compute scaling
+        AffineTransform3D sca = AffineTransform3D.createScaling(radius, radius, length);
+        
+        // rotation
+        
+        // convert pair of points to spherical coordinates
+        AffineTransform3D rot = AffineTransform3D.fromMatrix(orientation().affineMatrix());
+        
+        // concatenate and translate
+        AffineTransform3D tra = AffineTransform3D.createTranslation(center);
+        return tra.concatenate(rot).concatenate(sca);
+    }
+    
+    private AffineTransform3D globalToLocalTransform()
+    {
+        return localToGlobalTransform().inverse();
+    }
+
+}
